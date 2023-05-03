3a09918a6d3080d290834ca7de8dad42b5203b97
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-03 12:21:42 +0000

    import { Container, Navbar, FormControl, Nav, Dropdown, Badge } from 'react-bootstrap'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, FormControl, Nav, Dropdown, Badge } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a>Shopping cart</a>
+          <a href="/">Shopping cart</a>
         </Navbar.Brand>
+        <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              type="search"
+              placeholder="Search a product..."
+              className="m-auto"
+              aria-label="Search" />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              {/* <FaShoppingCart color="white" fontSize="25px" /> */}
+              <Badge>10</Badge>
+            </Dropdown.Toggle>
+
+             <Dropdown.Menu style={{ minWidth: 370 }}>
+               <span style={{padding: 10}}>cart is empty</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
       </Container>
     </Navbar>
   );
