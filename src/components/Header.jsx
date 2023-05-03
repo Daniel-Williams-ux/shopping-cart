824e6aac336fb04c520f91b82acb2a1383f833dd
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-03 07:31:32 +0000

    import { Container, Navbar } from 'react-bootstrap'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { Container, Navbar } from 'react-bootstrap';
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a>Shopping cart</a>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
